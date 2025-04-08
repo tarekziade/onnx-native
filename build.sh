13e32e121515289ee37b88e7fdfed45e29426c1e
Paul Adenot <paul@paul.cx>
2025-04-08 13:54:53 +0200

    Generalized build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-git clone --recursive https://github.com/Microsoft/onnxruntime.git
+#!/bin/bash -xe
+
+[ -d onnxruntime/ ] || git clone --recursive https://github.com/Microsoft/onnxruntime.git
 cd onnxruntime
 python3 -m venv .
 . bin/activate
 pip install -r requirements-dev.txt
-python tools/ci_build/build.py --build_dir build/MacOS --config RelWithDebInfo --build_shared_lib --parallel --compile_no_warning_as_error --skip_submodule_sync --cmake_extra_defines CMAKE_OSX_ARCHITECTURES=arm64 --disable_exceptions --disable_rtti --skip_tests --path_to_protoc_exe /opt/homebrew/bin/protoc
-cp build/MacOS/RelWithDebInfo/libonnxruntime.1.22.0.dylib ../
+BUILD_TYPE=Release
+# CMAKE_OSX_ARCHITECTURES is ignored on non-macOS
+python tools/ci_build/build.py --build_dir build/ --config $BUILD_TYPE --build_shared_lib --parallel --compile_no_warning_as_error --skip_submodule_sync --cmake_extra_defines CMAKE_OSX_ARCHITECTURES=arm64 --disable_exceptions --disable_rtti --skip_tests
+cp build/$BUILD_TYPE/libonnxruntime.so.1.22.0 ../libonnxruntime.so
